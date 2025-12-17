886aa286ac7d9ac9116b5f3abebf075b82873f6d
Ferxal の PC <ferxalhhh@gmail.com>
2025-12-17 21:24:08 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -630,10 +630,10 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
+	enabled: true, // 是否显示Umami统计
 	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: "https://api.umami.is", // Umami Cloud API地址
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="2cf65cda-3884-4d50-a29c-5091a2a474c2"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
